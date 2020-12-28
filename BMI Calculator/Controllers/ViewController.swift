bdcce93a37dbb9672cc75c8fd7f6ce2761ed740e
Austin Brovick <austinbrovick@gmail.com>
2020-12-27 18:36:43 -0800

    check point 1

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    var height: Float? = nil
+    var weight: Int? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightChanged(_ sender: UISlider) {
+        print(String(format: "%.2f", sender.value))
+        heightLabel.text = "\(String(format: "%.2f", sender.value))m"
+    }
+    
+    @IBAction func weightChanged(_ sender: UISlider) {
+        weightLabel.text = "\(Int(sender.value))Kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        print("button pressed")
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        print(height)
+        print(weight)
+        
+        let bmi = weight / pow(height, 2)
+        print(bmi)
+    }
+    
 }
 
